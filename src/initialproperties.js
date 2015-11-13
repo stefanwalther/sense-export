5db5bf87a1973edc4cfd9d641b8e78ca5625fcf5
Stefan Walther <swr@qlik.com>
2015-11-13 03:20:20 +0100

    Initial commit.

---
+++ src/initialproperties.js
@@ -0,0 +1,16 @@
+/*global define*/
+define( [], function () {
+  'use strict';
+  return {
+    qHyperCubeDef: {
+      qDimensions: [],
+      qMeasures: [],
+      qInitialDataFetch: [
+        {
+          qWidth: 20,
+          qHeight: 500
+        }
+      ]
+    }
+  };
+} );
---
+++ src/swr-sense-export.js
@@ -0,0 +1,67 @@
+/*global define*/
+define( [
+		'qlik',
+		'./properties',
+		'./initialproperties',
+			'text!./lib/css/main.css',
+		'text!./template.ng.html',
+		'./lib/external/sense-extension-utils/extUtils',
+
+		// components
+		'./lib/components/eui-button/eui-button',
+		'./lib/components/eui-overlay/eui-overlay',
+		'./lib/components/eui-simple-table/eui-simple-table'
+	],
+	function ( qlik, props, initProps, cssContent, ngTemplate, extUtils ) {
+		'use strict';
+
+		extUtils.addStyleToHeader( cssContent );
+		var faUrl = '/extensions/swr-sense-export/lib/external/fontawesome/css/font-awesome.min.css';
+		extUtils.addStyleLinkToHeader( faUrl, 'swr_sense_export__fontawesome' );
+
+		return {
+			definition: props,
+			initialProperties: initProps,
+			snapshot: {canTakeSnapshot: false},
+			template: ngTemplate,
+			controller: ['$scope', function ( $scope ) {
+
+				$scope.$watchCollection( 'layout.props', function ( newVals, oldVals ) {
+					Object.keys( newVals ).forEach( function ( key ) {
+						if ( newVals[key] !== oldVals[key] ) {
+							console.log(key, newVals[key]);
+							$scope[key] = newVals[key];
+						}
+					} );
+				} );
+
+
+				$scope.showUnsupportedOverlay = function (  ) {
+					return qlik.table === undefined;
+				};
+				$scope.debug = function (  ) {
+					return ($scope.layout.props.isDebug === true);
+				};
+
+
+				$scope.export = function () {
+
+					var exportOpts = {
+						format: $scope.layout.props.exportFormat,
+						state: $scope.layout.props.exportState,
+						fileName: $scope.layout.props.exportFileName,
+						download: true
+					};
+
+					if (qlik.table) {
+						var qTable = qlik.table(this);
+						qTable.exportData( exportOpts );
+					} else {
+
+					}
+
+				}
+
+			}]
+		};
+	} );
