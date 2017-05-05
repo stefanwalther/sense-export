4d0072299c0b8304bd94ec0ccbf7961bf699fe78
Stefan Walther <swr@selun-swr-mac.fritz.box>
2017-05-05 22:20:38 +0200

    Bump v1.1.0 - wbfolder.wbl file in dev release

--- build/dev/swr-sense-export.js
+++ build/dev/swr-sense-export.js
@@ -2,7 +2,7 @@
 
 * sense-export - Just a simple button to export data in your Qlik Sense application without displaying them in a table first.
 *
-* @version v1.0.4
+* @version v1.1.0
 * @link 
 * @author [object Object]
 * @license MIT
